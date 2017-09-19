c1660e858f5a96c742e37e08287e2f64bdc3672e
Daniel Jones <tortxof@gmail.com>
2017-09-19 01:58:23 -0400

    Don't need aws keys on lambda

--- app.py
+++ app.py
@@ -9,16 +9,12 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['DEBUG'] = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
-app.config['AWS_ACCESS_KEY_ID'] = os.environ.get('APP_AWS_ACCESS_KEY_ID')
-app.config['AWS_SECRET_ACCESS_KEY'] = os.environ.get('APP_AWS_SECRET_ACCESS_KEY')
 app.config['AWS_SNS_TOPIC'] = os.environ.get('AWS_SNS_TOPIC')
 app.config['AWS_SNS_REGION'] = os.environ.get('AWS_SNS_REGION')
 app.config['APP_TZ'] = os.environ.get('APP_TZ')
 
 sns = boto3.resource(
     'sns',
-    aws_access_key_id = app.config['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key = app.config['AWS_SECRET_ACCESS_KEY'],
     region_name = app.config['AWS_SNS_REGION'],
 )
 topic = sns.Topic(app.config['AWS_SNS_TOPIC'])
